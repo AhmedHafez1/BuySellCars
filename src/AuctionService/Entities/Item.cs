f2fe69f227e27ad6dd472ac486503bee5986fd46
EMEA\IBA1SF <fixed-term.ahmad.ibraheem@bosch.com>
2024-10-17 16:28:27 +0300

    Entities

---
+++ src/AuctionService/Entities/Item.cs
@@ -0,0 +1,16 @@
+namespace AuctionService.Entities;
+
+public class Item
+{
+    public Guid Id { get; set; }
+    public string Make { get; set; } = default!;
+    public string Model { get; set; } = default!;
+    public int Year { get; set; }
+    public string Color { get; set; } = default!;
+    public int Mileage { get; set; }
+    public string ImageUrl { get; set; } = default!;
+
+    // Nav Props
+    public Auction Auction { get; set; } = default!;
+    public int AuctionId { get; set; }
+}
