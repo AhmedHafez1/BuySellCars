3d58b162b93aab7a88d9abf9fd2bb245fed23e00
A.Hafez <ahmed.abdelghany.hafez@gmail.com>
2024-10-19 08:21:21 +0300

    AuctionsController

--- src/AuctionService/Program.cs
+++ src/AuctionService/Program.cs
@@ -4,22 +4,18 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddDbContext<AuctionDbContext>(opt =>
 {
     opt.UseNpgsql(builder.Configuration.GetConnectionString("Default"));
 });
-
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddControllers();
 
 var app = builder.Build();
 
 // Configure the Http request pipeline.
-
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
 
 try
