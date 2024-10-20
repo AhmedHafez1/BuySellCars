a005bb2df2bf1013a3a0fb334fe03c85377778e7
A.Hafez <ahmed.abdelghany.hafez@gmail.com>
2024-10-20 07:41:21 +0300

    SearchParams

--- src/SearchService/Controllers/SearchController.cs
+++ src/SearchService/Controllers/SearchController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using MongoDB.Entities;
 using SearchService.Models;
+using SearchService.RequestHelper;
 
 namespace SearchService.Controllers
 {
@@ -9,18 +10,54 @@ namespace SearchService.Controllers
     public class SearchController : ControllerBase
     {
         [HttpGet]
-        public async Task<ActionResult<List<Item>>> SearchItems(string searchTerm = "")
+        public async Task<IActionResult> SearchItems([FromQuery] SearchParams searchParams)
         {
-            var query = DB.Find<Item>().Sort(x => x.Ascending(a => a.Make));
+            var query = DB.PagedSearch<Item, Item>();
 
-            if (!string.IsNullOrWhiteSpace(searchTerm))
+            if (!string.IsNullOrWhiteSpace(searchParams.SearchTerm))
             {
-                query.Match(Search.Full, searchTerm).SortByTextScore();
+                query.Match(Search.Full, searchParams.SearchTerm).SortByTextScore();
             }
 
-            var items = await query.ExecuteAsync();
+            query = searchParams.FilterBy switch
+            {
+                "finished" => query.Match(x => x.AuctionEnd < DateTime.UtcNow),
+                "endingSoon" => query.Match(x =>
+                    x.AuctionEnd < DateTime.UtcNow.AddHours(12) && x.AuctionEnd > DateTime.UtcNow
+                ),
+                _ => query.Match(x => x.AuctionEnd > DateTime.UtcNow),
+            };
 
-            return items;
+            if (!string.IsNullOrWhiteSpace(searchParams.Seller))
+            {
+                query.Match(x => x.Seller == searchParams.Seller);
+            }
+
+            if (!string.IsNullOrWhiteSpace(searchParams.Winner))
+            {
+                query.Match(x => x.Winner == searchParams.Winner);
+            }
+
+            query = searchParams.OrderBy switch
+            {
+                "make" => query.Sort(x => x.Ascending(a => a.Make)),
+                "new" => query.Sort(x => x.Descending(a => a.CreatedAt)),
+                _ => query.Sort(x => x.Ascending(a => a.AuctionEnd)),
+            };
+
+            query.PageNumber(searchParams.PageNumber);
+            query.PageSize(searchParams.PageSize);
+
+            var result = await query.ExecuteAsync();
+
+            return Ok(
+                new
+                {
+                    results = result.Results,
+                    pageCount = result.PageCount,
+                    totalCount = result.TotalCount,
+                }
+            );
         }
     }
 }
