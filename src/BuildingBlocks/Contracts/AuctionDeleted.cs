cb42f3f161a8ee1561a4f3092b41f116378a100d
A.Hafez <ahmed.abdelghany.hafez@gmail.com>
2024-10-22 06:48:16 +0300

    Events

---
+++ src/BuildingBlocks/Contracts/AuctionDeleted.cs
@@ -0,0 +1,6 @@
+namespace BuildingBlocks.Contracts;
+
+public class AuctionDeleted
+{
+    public string Id { get; set; } = default!;
+}
---
+++ src/BuildingBlocks/Contracts/AuctionUpdated.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace BuildingBlocks.Contracts;
+
+public class AuctionUpdated
+{
+    public string Id { get; set; } = default!;
+
+    public string? Make { get; set; } = default!;
+
+    public string? Model { get; set; } = default!;
+
+    public int? Year { get; set; }
+
+    public string? Color { get; set; } = default!;
+
+    public int? Mileage { get; set; }
+}
