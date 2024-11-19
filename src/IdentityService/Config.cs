29a65c55f7e474116a1ef4a279ece447852ffb20
A.Hafez <ahmed.abdelghany.hafez@gmail.com>
2024-11-19 06:40:04 +0200

    Next Client

--- src/IdentityService/Config.cs
+++ src/IdentityService/Config.cs
@@ -21,5 +21,17 @@ public static class Config
                 ClientSecrets = [new Secret("secret".Sha256())],
                 AllowedGrantTypes = { GrantType.ResourceOwnerPassword },
             },
+            new Client
+            {
+                ClientId = "nextApp",
+                ClientName = "Next.js Application",
+                ClientSecrets = { new Secret("secret".Sha256()) },
+                AllowedGrantTypes = GrantTypes.CodeAndClientCredentials,
+                RequirePkce = false,
+                RedirectUris = { "http://localhost:3000/api/auth/callback/id-server" },
+                AllowOfflineAccess = true,
+                AllowedScopes = { "openid", "profile", "auctionApp" },
+                AccessTokenLifetime = 3600 * 24 * 30,
+            },
         ];
 }
